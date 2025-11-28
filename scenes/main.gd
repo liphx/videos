bd4b1b9beaf945f26f277371a43b62a70fa0c9fd
liph <lipenghua@lipenghua.com>
2025-11-28 11:40:43 +0800

    update

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,18 +1,32 @@
 extends Control
 
-var item_scene = preload("res://scenes/item.tscn")
 @onready var container = $MarginContainer/VBoxContainer/ScrollContainer/CenterContainer/GridContainer
+var item_scene = preload("res://scenes/item.tscn")
+var _items = [] # [{'path': path, 'meta': meta, 'instance': instance}]
+const sort_options = [
+    '默认排序',
+    '按名称排序',
+    '按年份排序',
+    '按年份排序（倒序）',
+]
+var _sort_index = 0      # 排序选项
+var _search_text = ''    # 搜索文本
 
-var _items = {}
+
+func setup_ui() -> void:
+    for option in sort_options:
+        $MarginContainer/VBoxContainer/HBoxContainer/sort.add_item(option)
+    $MarginContainer/VBoxContainer/HBoxContainer/sort.selected = 0
+
 
 func _ready() -> void:
+    setup_ui()
     load_items()
+    display_items()
 
 
 func load_items() -> void:
     _items.clear()
-    for child in container.get_children():
-        child.queue_free()
 
     var videos_path = Config.get_value('VIDEOS_PATH')
     print('videos_path: ', videos_path)
@@ -45,12 +59,40 @@ func load_items() -> void:
             meta.merge(info, true)
             meta.douban_update_at = Datetime.timestamp_now()
             FileUtil.save_json(meta_path, meta)
+        _items.append({
+            'path': path,
+            'meta': meta
+        })
+
+
+func display_items() -> void:
+    for child in container.get_children():
+        child.queue_free()
+    if _sort_index == 0:
+        _items.sort_custom(func(a, b):
+            return int(a['meta']["douban"]) < int(b['meta']["douban"])
+        )
+    elif _sort_index == 1:
+        _items.sort_custom(func(a, b):
+            return a['meta']["title"] < b['meta']["title"]
+        )
+    elif _sort_index == 2:
+        _items.sort_custom(func(a, b):
+            return a['meta']["year"] < b['meta']["year"]
+        )
+    elif _sort_index == 3:
+        _items.sort_custom(func(a, b):
+            return a['meta']["year"] > b['meta']["year"]
+        )
+    for item in _items:
+        var meta = item['meta']
+        if _search_text != '' and not meta['title'].containsn(_search_text):
+            continue
         var instance = item_scene.instantiate()
         instance.data = meta
-        instance.data.local_path = path
+        instance.data.local_path = item['path']
         container.add_child(instance)
-        _items[path] = meta
-        _items[path]['instance'] = instance
+        item['instance'] = instance
 
 
 func _on_settings_pressed() -> void:
@@ -60,18 +102,14 @@ func _on_settings_pressed() -> void:
 func _on_file_dialog_dir_selected(dir: String) -> void:
     Config.set_value('VIDEOS_PATH', dir)
     load_items()
+    display_items()
 
 
 func _on_search_pressed() -> void:
-    var text = $MarginContainer/VBoxContainer/HBoxContainer/search_text.text.strip_edges()
-    print('search_text: ', text)
-    if text == '': # 重置
-        for path in _items:
-            var instance = _items[path]['instance']
-            instance.show()
-        return
-    for path in _items:
-        var instance = _items[path]['instance']
-        instance.hide()
-        if _items[path]['title'].containsn(text):
-            instance.show()
+    _search_text = $MarginContainer/VBoxContainer/HBoxContainer/search_text.text.strip_edges()
+    display_items()
+
+
+func _on_sort_item_selected(index: int) -> void:
+    _sort_index = index
+    display_items()
